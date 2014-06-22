08a5d929e59c21211896e5760efdac92b205cead
wataori <ropifuku@gmail.com>
2014-06-22 13:49:23 +0900

    spec7: recipes have a url

--- memo.md
+++ memo.md
@@ -1,6 +1,12 @@
-#工夫したこと
-##仕様5
+##工夫したこと
+###仕様5
 [レシピ一覧を保持する配列を準備して、複数のレシピとレシピIDの関係を分かりやすくした](https://github.com/suiyujin/suiyujin-sezemi-2014-readable-code-1/commit/86b17e01091296db1cd16ce6d8a835b20f4ae4c7 "spec5: complete spec5")
 
-##仕様6
+###仕様6
 [ファイルのデータを配列に格納する処理を汎用化した](https://github.com/suiyujin/suiyujin-sezemi-2014-readable-code-1/commit/eeaa46d13c68949087d8f444a4309767adc7d3f8 "spec6: modified README.md")
+
+
+##いいと思ったこと
+* 引数`ARGV[*]`をわかりやすく名前をつけた変数に代入していたこと
+* わかりやすいようコメントを書いてあったこと
+* ファイルを開く際に"r"オプションをつけて、目的がファイルの読み込みであることを明示していたこと
--- recipe.rb
+++ recipe.rb
@@ -1,9 +1,12 @@
+# coding: utf-8
+
 # ファイルを行ごとのデータ配列に格納する
 def insert_array_from_line_of_file(file)
     data_array = Array.new
 
     File.open(file, "r") do |f|
         while line = f.gets
+            #line = line.split(' ')
             data_array.push(line)
         end
     end
@@ -13,7 +16,7 @@ end
 
 # 引数からレシピのデータファイルと指定されたレシピIDを取得
 recipe_data_file = ARGV[0]
-select_recipe_id = ARGV[1]
+recipe_id = ARGV[1]
 
 # レシピ一覧用の配列を準備
 recipes = Array.new
@@ -25,9 +28,12 @@ recipes = insert_array_from_line_of_file(recipe_data_file)
 recipes.each.with_index(1) do |name, id|
     # レシピIDが指定されている場合は
     # 指定されているID以外をスキップする
-    if select_recipe_id && select_recipe_id.to_i != id then
+    if recipe_id && recipe_id.to_i != id then
         next
     end
 
     puts "#{id}: #{name}"
 end
+
+#s = 'オムライス http://cookpad.com/recipe/2653946'
+#p s.split
