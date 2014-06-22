86b17e01091296db1cd16ce6d8a835b20f4ae4c7
suiyujin <suiyujin04@gmail.com>
2014-06-22 11:37:52 +0900

    spec5: complete spec5

--- recipe.rb
+++ recipe.rb
@@ -1,9 +1,17 @@
+# レシピ一覧用の配列を準備
+recipes = Array.new
+
 # 引数からレシピのデータファイルを取得
 recipe_data_file = ARGV[0]
 
-# データファイルの内容を出力
+# データファイルからレシピ一覧を作成
 File.open(recipe_data_file, "r") do |f|
-    while recipe = f.gets
-        puts recipe
+    while name = f.gets
+        recipes.push(name)
     end
+end
+
+# レシピを一つずつ出力
+recipes.each.with_index(1) do |name, id|
+    puts "#{id}: #{name}"
 end
